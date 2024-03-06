e9f97d35e17c5fbc3d05299fcc4270a8ee909803
Danisole <danisole82@gmail.com>
2024-03-05 23:55:04 -0300

    margenes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <div className=" bg-neutralSilver" id="home">
       <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen flex justify-center items-center">
         <Carousel className="w-full mx-auto">
-          <div className="my-8 md:my-8 py-12 flex flex-col w-full mx-auto md:flex-row-reverse items-center justify-between gap-12">
+          <div className="my-16 md:my-8 py-12 flex flex-col w-full mx-auto md:flex-row-reverse items-center justify-between gap-12">
           <div>
             <img src={banner} alt=""/>
            </div>
@@ -22,7 +22,7 @@ const Home = () => {
             <button className="px-7 py-2 bg-brandPrimary text-white rounded-lg hover:bg-neutralDGrey">Registrate</button>
            </div>
           </div>
-          <div className="my-8 md:my-8 py-12 flex flex-col w-full mx-auto md:flex-row-reverse items-center justify-between gap-12">
+          <div className="my-16 md:my-8 py-12 flex flex-col w-full mx-auto md:flex-row-reverse items-center justify-between gap-12">
           <div>
             <img src={banner2} alt=""/>
            </div>
@@ -33,7 +33,7 @@ const Home = () => {
             <button className="px-7 py-2 bg-brandPrimary text-white rounded-lg hover:bg-neutralDGrey">Contactanos</button>
            </div>
           </div>
-          <div className="my-18 md:my-8 py-12 flex flex-col w-full mx-auto md:flex-row-reverse items-center justify-between gap-12">
+          <div className="my-16 md:my-8 py-12 flex flex-col w-full mx-auto md:flex-row-reverse items-center justify-between gap-12">
           <div>
             <img src={banner3} alt=""/>
            </div>
